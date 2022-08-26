4599f7f317507710896eaadc5c5479768d2194c6
Hericson Ramos <hericson@bwfti.com.br>
2022-08-25 21:13:16 -0300

    First commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import { loadFonts } from './plugins/webfontloader'
+import 'vuetify/styles' // Global CSS has to be imported
+const vuetify = createVuetify() // Replaces new Vuetify(...)
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
