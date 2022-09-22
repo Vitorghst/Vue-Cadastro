9f928c99d82d13c783525d01caa3e27ef95d466a
Vitorghst <vitor4023@edu.faccar.com.br>
2022-09-22 19:33:28 -0300

    Implementado o formulario de cadastro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -196,33 +196,35 @@
            v-bind:class="{'form-control': true, 'is-invalid': !validEstado(estado) && estadoBlured, 'is-valid': validEstado(estado)}"
            v-on:blur="estadoBlured = true">
               <option value="">Selecione</option>
-              <option value="1">Acre</option>
-              <option value="2">Alagoas</option>
-              <option value="3">Amapá</option>
-              <option value="4">Amazonas</option>
-              <option value="5">Bahia</option>
-              <option value="6">Ceará</option>
-              <option value="7">Distrito Federal</option>
-              <option value="8">Espírito Santo</option>
-              <option value="9">Goiás</option>
-              <option value="10">Maranhão</option>
-              <option value="11">Mato Grosso</option>
-              <option value="12">Mato Grosso do Sul</option>
-              <option value="13">Minas Gerais</option>
-              <option value="14">Pará</option>
-              <option value="15">Paraíba</option>
-              <option value="16">Paraná</option>
-              <option value="17">Pernambuco</option>
-              <option value="18">Piauí</option>
-              <option value="19">Rio de Janeiro</option>
-              <option value="20">Rio Grande do Norte</option>
-              <option value="21">Rio Grande do Sul</option>
-              <option value="22">Rondônia</option>
-              <option value="23">Roraima</option>
-              <option value="24">Santa Catarina</option>
-              <option value="25">São Paulo</option>
-              <option value="26">Sergipe</option>
-              <option value="27">Tocantins</option>
+              <option value="1">AC</option>
+              <option value="2">AL</option>
+              <option value="3">AP</option> 
+              <option value="4">AM</option>
+              <option value="5">BA</option>
+              <option value="6">CE</option>
+              <option value="7">DF</option>
+              <option value="8">ES</option>
+              <option value="9">GO</option>
+              <option value="10">MA</option>
+              <option value="11">MT</option>
+              <option value="12">MS</option>
+              <option value="13">MG</option>
+              <option value="14">PA</option>
+              <option value="15">PB</option>
+              <option value="16">PR</option>
+              <option value="17">PE</option>
+              <option value="18">PI</option>
+              <option value="19">RJ</option>
+              <option value="20">RN</option>
+              <option value="21">RS</option>
+              <option value="22">RO</option>
+              <option value="23">RR</option>
+              <option value="24">SC</option>
+              <option value="25">SP</option>
+              <option value="26">SE</option>
+              <option value="27">TO</option>
+              
+              
             </select>
             <div class="invalid-feedback">"Estado" não pode ficar em branco.</div>
           </div>
